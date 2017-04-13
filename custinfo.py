d7a033eaccebb4826dc77deeab5901e7460c0423
Interplay <machinate@live.com>
2017-04-13 00:59:06 -0400

    custinfo fixed some strings

--- custinfo.py
+++ custinfo.py
@@ -58,6 +58,6 @@ while os.path.exists("CustOrder%s.txt" % i):
     o += 1
 
 fh = open("CustOrder%s.txt" % i, "w")
-rs = ("\n"+name+"has spent a total of "+ str(custspend) +"on Order Number: %s" %o+ "\n")
+rs = ("\n"+name+"has spent a total of $"+ str(custspend) +" on Order Number: %s" %o+ "\n")
 fh.writelines(rs)
 fh.close()
